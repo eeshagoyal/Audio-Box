5a3bdafe29a6f00ed8fc64a2cd29774220a7c04a
Eesha Goyal <eeshagoyal96@gmail.com>
2018-06-04 19:56:34 +0530

    new UI

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const albumUrl = "http://www.theaudiodb.com/api/v1/json/1/album.php?i=";
+const trackUrl = "http://www.theaudiodb.com/api/v1/json/1/track.php?m=";
+
+
+
+document.getElementByID("app").innerHTML= 'Hello';
+/*
+$(document).ready(function(){
+    $("#submit").on('enter'||'click'(function(e){
+        alert("You entered "+('#submit').val());
+    });
+});
+*/
+
