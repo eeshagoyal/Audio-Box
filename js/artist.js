c593162fa993b2a008d47ebf6368b7a1b6fd89d2
Eesha Goyal <eeshagoyal96@gmail.com>
2018-06-05 18:51:47 +0530

    Added view all albums functionality

--- js/artist.js
+++ js/artist.js
@@ -1,80 +1,77 @@
 const artistURL = "http://www.theaudiodb.com/api/v1/json/1/search.php?s=";
-const albumUrl = "http://www.theaudiodb.com/api/v1/json/1/album.php?i=";
-const trackUrl = "http://www.theaudiodb.com/api/v1/json/1/track.php?m=";
+const albumURL = "http://www.theaudiodb.com/api/v1/json/1/album.php?i=";
+const trackURL = "http://www.theaudiodb.com/api/v1/json/1/track.php?m=";
 
-var SearchInput = document.getElementById("search-bar");
-var submit = document.getElementById("search-button");
-/*
+ var url = window.location.href;
+ var qparts = url.split("?");
 
-function ArtistPage(){
-	console.log('in artist fn');
-	location.href = "artist.html";
+ if(qparts.length > 0 ){
+ 	ArtistID = qparts[1];
+ 	console.log(ArtistID);
+ }
+ else
+ 	console.log('ERROR : no artist ID found.');
+
+loadcards(ArtistID);
+
+
+function Tracks(AlbumID){
+	console.log(AlbumID);
 }
 
-
-function renderList(obj){
+function renderList(obj) {
 	return `
 		<li class="result_item">
 			<div class="result_item__image">
-				<img src ="${obj.strArtistThumb}" height="100" width="100"/>  
+				<img src ="${obj.strAlbumThumb}" height="100" width="100"/>  
 			</div>
 
 			<div class="result_item__content">
 				<p class="text--medium">
-					${obj.strArtist}
+					${obj.strAlbum}
 				</p>
-				<p class="text--muted"> ${obj.intFormedYear}</p>
+				<p class="text--muted"> ${obj.intYearReleased}</p>
 				<p class="text--muted"> ${obj.strGenre}</p>
 			</div>
 
-			<div id="ViewAlbums" class="result_item__button">
-				<button class="button"> View Albums </button>
+			<div class="result_item__button">
+				<button class="button" onclick="Tracks(${obj.idAlbum});"> View Tracks </button>
 			</div>
 		</li>
 	`;
 }
 
 
-function loadcards(){
-
-	console.log(SearchInput.value);
+function loadcards(event) {
 
 	var request = new XMLHttpRequest();
-	//
-	request.open('GET',artistURL + SearchInput.value );
+	request.open('GET', albumURL + ArtistID, true); 
+	//async=true
+	
+	
+	//CONTENT TYPE - main errors !
+	request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+	request.setRequestHeader('Accept', 'application/json');
+
+	request.send();
 
 	request.onload = function () {
+
 		var data = JSON.parse(request.responseText);
-		console.log(data["artists"][0].strArtist);
+		console.log(data["album"][0].strAlbum);
 
 		document.getElementById("result-list").innerHTML = `
-			<ul class="card">
-				${data["artists"].map(renderList).join('')}
-			</ul>
-
-			<div class="text--center">
-			  <p class="text--muted">No more results.</p>
-			</div>	
-			`;
+					<ul class="card">
+						${data["album"].map(renderList).join('')}
+					</ul>
 		
-		ViewAlbums.addEventListener("click",ArtistPage,false);
-
+					<div class="text--center">
+						<p class="text--muted">No more results.</p>
+					</div>	
+				`;
 	}
-	request.send();
+	request.onerror = function () {
+		console.log(request);
+	}
 }
-
-
-SearchInput.addEventListener("keyup", function(event){
-	// Cancel the default action, if needed
-  	event.preventDefault();
-  	// Number 13 is the "Enter" key on the keyboard
-  	if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    	submit.click();
-	}
-},false);
-
-submit.addEventListener("click", loadcards,false);
-
-*/
 
